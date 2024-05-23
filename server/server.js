af1f551c2fbb16d43cfd673b94a3eb7cc4edd04e
Vitalii-fedorov <fva23322@gmail.com>
2024-05-23 12:24:01 +0300

    build

--- server/server.js
+++ server/server.js
@@ -71,7 +71,7 @@ const createOrder = async (cart) => {
       Authorization: `Bearer ${accessToken}`,
       // Uncomment one of these to force an error for negative testing (in sandbox mode only). Documentation:
       // https://developer.paypal.com/tools/sandbox/negative-testing/request-headers/
-      // "PayPal-Mock-Response": '{"mock_application_codes": "MISSING_REQUIRED_PARAMETER"}'
+      "PayPal-Mock-Response": '{"mock_application_codes": "MISSING_REQUIRED_PARAMETER"}'
       // "PayPal-Mock-Response": '{"mock_application_codes": "PERMISSION_DENIED"}'
       // "PayPal-Mock-Response": '{"mock_application_codes": "INTERNAL_SERVER_ERROR"}'
     },
